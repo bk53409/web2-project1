906a3db74a204a43d9c6eb2023c0b34824d535bb
bk53409 <bk53409@fer.hr>
2023-10-27 11:52:55 +0200

    init - 2

---
+++ src/main/java/com/auth0/example/model/Korisnik.java
@@ -0,0 +1,36 @@
+package com.auth0.example.model;
+
+public class Korisnik {
+    private Long id;
+    private String name;
+    private String email;
+
+    public Korisnik(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
